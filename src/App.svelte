17cc77884f43bc79fbb442f24e68a4984d183916
Devanshu Suthar <w0822400@myscc.ca>
2025-01-20 13:36:52 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import SecondButton from "./components/SecondButton.svelte";
+
+  let number = 1;
+
+  function incrementNumber() {
+    number++;
+  }
+</script>
+
+<button on:click={incrementNumber}>Click Me: {number}</button>
+<SecondButton />
+
+<style>
+  button {
+    padding: 1rem;
+  }
+</style>

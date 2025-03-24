a679bbbb241d61cafd9f24370ac65d9855389cbd
Devanshu Suthar <w0822400@myscc.ca>
2025-03-24 12:36:45 -0400

    Wires frontend done.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   import Login from "./pages/MeetingConclusion.svelte";
   import TaskPage from "./pages/TaskPage.svelte";
+  import Calibrate from "./tasks/calibrates distribution/Calibrate.svelte";
   import Switches from "./tasks/switches/Switches.svelte";
   import Wires from "./tasks/wires/Wires.svelte";
 </script>
 
 <TaskPage>
-  <Wires />
+  <Calibrate />
 </TaskPage>

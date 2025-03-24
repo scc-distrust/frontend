8ec15634cb71ea84f8750fa4385461f3ebd3deb2
Devanshu Suthar <w0822400@myscc.ca>
2025-03-24 12:23:46 -0400

    Created colored boxes.

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,9 @@
   import Login from "./pages/MeetingConclusion.svelte";
   import TaskPage from "./pages/TaskPage.svelte";
   import Switches from "./tasks/switches/Switches.svelte";
+  import Wires from "./tasks/wires/Wires.svelte";
 </script>
 
 <TaskPage>
-  <Switches />
+  <Wires />
 </TaskPage>
---
+++ src/tasks/wires/Wires.svelte
@@ -0,0 +1,52 @@
+<div class="container">
+  <div class="container-1">
+    <div class="box red"></div>
+    <div class="box green"></div>
+    <div class="box blue"></div>
+    <div class="box purple"></div>
+  </div>
+  <div class="container-2">
+    <div class="box red"></div>
+    <div class="box green"></div>
+    <div class="box blue"></div>
+    <div class="box purple"></div>
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+
+  .container-1,
+  .container-2 {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .box {
+    width: 50px;
+    height: 50px;
+    border-radius: 8px;
+  }
+
+  .red {
+    background-color: var(--red);
+  }
+  .green {
+    background-color: var(--green);
+  }
+  .blue {
+    background-color: var(--cyan);
+  }
+  .purple {
+    background-color: var(--purple);
+  }
+</style>
